6e78f3e8efda0860bb5fe29c1fad66a57d7ee377
唐志勇 <ward@tangzhiyongdeMacBook-Pro.local>
2019-08-28 23:08:38 +0800

    first

---
+++ js/Test.js
@@ -0,0 +1,25 @@
+
+
+
+
+window.onscroll = function () {
+            // 滚动时当前位置距顶部的距离
+            var topScroll = document.documentElement.scrollTop;
+            console.log(topScroll)
+            // 获取导航id
+            var nav = document.getElementById("nav");
+            // 滚动距离大于多少时执行下面事件
+            if (topScroll > 369) {
+                // console.log(ihkh)
+                // 到了那个距离相当于给了导航定位
+                nav.style.position = 'fixed';
+                nav.style.top = '0';
+                nav.style.zIndex = '99'
+            } else {
+                // 小于的时候让他恢复原状
+                nav.style = ''
+            }
+        }
+
+
+    
