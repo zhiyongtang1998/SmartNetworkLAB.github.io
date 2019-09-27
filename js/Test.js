54d99f6f858e137f20de8dad8ae0e82191c11f9c
SmartNetworkLAB <1134912237@qq.com>
2019-09-27 20:55:05 +0100

    更新

--- js/Test.js
+++ js/Test.js
@@ -9,7 +9,7 @@ window.onscroll = function () {
             // 获取导航id
             var nav = document.getElementById("nav");
             // 滚动距离大于多少时执行下面事件
-            if (topScroll > 369) {
+            if (topScroll > 0) {
                 // console.log(ihkh)
                 // 到了那个距离相当于给了导航定位
                 nav.style.position = 'fixed';
